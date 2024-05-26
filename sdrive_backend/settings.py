70b8a5f10572acbeae86a0d3babc96b98ed12e68
Sumit Kumar <rsumit123@gmail.com>
2024-05-26 12:05:42 +0530

    Updated admin page header and static files

--- sdrive_backend/settings.py
+++ sdrive_backend/settings.py
@@ -159,7 +159,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/sdrive_api.skdev.one/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- sdrive_backend/urls.py
+++ sdrive_backend/urls.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+admin.site.site_header = (
+    "SDrive Administration"  # default: "Django Administration"
+)
+admin.site.index_title = "SDrive Administration"  # default: "Site administration"
+admin.site.site_title = (
+    "Welcome to SDrive Management Portal"  # default: "Django site admin"
+)
+
+admin.sites.AdminSite.site_header = "SDrive Administration"
+admin.sites.AdminSite.site_title = "SDrive Administration"
+admin.sites.AdminSite.index_title = "Welcome to SDrive Management Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('files.urls')),  # Ensure this line includes 'files.urls'
